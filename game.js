fe88168b182b2f61e854d2a9345b1c6f59258fdc
Felipe Serrou Camy de Paiva <82775159+Feguri@users.noreply.github.com>
2023-12-14 23:19:25 -0500

    working anim

--- game.js
+++ game.js
@@ -25,7 +25,7 @@ winImage.onload = function () {
 // Player image
 var playerReady = false;
 var playerImage = new Image(); 
-playerImage.src = "images/Walkcycles.png"; 
+playerImage.src = "images/spritesheetBIG.png"; 
 playerImage.onload = function () {
     playerReady = true; 
 };
@@ -40,9 +40,9 @@ goodyImage.onload = function () {
 
 // Create global game objects 
 var player = {
-    speed: 10, // movement in pixels per tick 
-    width: 50,
-    height: 50,
+    speed: 7, // movement in pixels per tick 
+    width: 100,
+    height: 100,
 };
 
 var goodies = [ // this is an array
@@ -61,6 +61,9 @@ let row = 0;
 
 let gAnim;
 
+let pos = 'back';
+let currKey = 'up';
+
 var velocity = {
     x: 0,
     y: 0
@@ -69,42 +72,51 @@ var velocity = {
 // Handle keyboard controls
 addEventListener("keydown", function (e) {
     //Keystrokes
+    currKey = 'down';
 
     if (!gAnim) {
-        gAnim = setInterval(anim,30);
+        gAnim = setInterval(anim,20);
     }
 
     if (e.keyCode == 38) { // UP
         vX = 0;
         vY = -player.speed;
         row=0;
+        pos = 'back';
     }
     if (e.keyCode == 40) { // DOWN
         vX = 0;
         vY = player.speed;
-        row=150;
+        row=300;
+        pos = 'front';
     }
     if (e.keyCode == 37) { // LEFT
         vX = -player.speed;
         vY = 0;
-        row=100;
+        row=200;
+        pos = 'left';
     }
     if (e.keyCode == 39) { // RIGHT
         vX = player.speed;
         vY = 0;
-        row=50;
+        row=100;
+        pos = 'right';
     }
     if (e.keyCode == 32) { // STOP spacebar
         vX = 0;
         vY = 0;
+        
     }
 }, false);
 
 addEventListener("keyup", function (e) {
+        currKey = 'up';
         vX = 0;
         vY = 0;
         clearInterval(gAnim);
-        gAnim = null;    
+        gAnim = null;
+        // change to default postition
+        ctx.drawImage(playerImage, 400, row);
 });
 
 // addEventListener("keyup", function (e) {
@@ -225,7 +237,14 @@ var render = function () {
     }
     if (playerReady) {
 			//ANIM Paint current frame of goodie, see anim() function below for managing the loop across spritesheet
-			ctx.drawImage(playerImage, gFrames*player.width, row, 50, 50, player.x, player.y, 50, 50);
+            if(currKey == 'down'){
+                ctx.drawImage(playerImage, gFrames*player.width, row, 100, 100, player.x, player.y, 100, 100);
+            } else {
+
+                ctx.drawImage(playerImage, 0*player.width, row, 100, 100, player.x, player.y, 100, 100);
+                
+            }
+			
 	}
     // ctx.drawImage(playerImage, 0, row, 50, 50, player.x, player.y, 100, 100);
 
@@ -244,6 +263,7 @@ var render = function () {
 
     // change this fill text for debugging
     ctx.fillText("Score: " + score, 32, 92);
+    ctx.fillText("Position status:"  + pos, 132, 192);   
 };
 
 //Generic function to check for collisions 
